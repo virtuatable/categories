bd95701d9667e7d60fc83aa1eac17f39fd244c44
Vincent Courtois <babaussine@gmail.com>
2018-02-06 03:14:17 +0100

    Cleaning and changing the name of the development database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'bundler'
 Bundler.require :test
 
 service = Arkaan::Utils::MicroService.instance
-  .register_as('permissions')
+  .register_as('categories')
   .from_location(__FILE__)
   .in_test_mode
