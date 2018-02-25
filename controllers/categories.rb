7cd3631c91a16d6879216132207b1362b4e48568
babausse <courtois.vincent@outlook.com>
2018-02-25 12:18:05 +0100

    Link the wiki to the routes

--- controllers/categories.rb
+++ controllers/categories.rb
@@ -1,12 +1,20 @@
 module Controllers
-  # Controller for the rights, mapped on /rights
+  # Controller for the categories of rights, mapped on /categories
   # @author Vincent Courtois <courtois.vincent@outlook.com>
   class Categories < Arkaan::Utils::Controller
-    declare_route 'get', '/' do
-      categories = Decorators::Category.decorate_collection(Arkaan::Permissions::Category.all)
-      halt 200, {count: Arkaan::Permissions::Category.count, items: categories.map(&:to_h)}.to_json
+
+    # @see https://github.com/jdr-tools/categories/wiki/Creation-of-a-category
+    declare_route 'post', '/' do
+      check_presence 'slug'
+      category = Arkaan::Permissions::Category.new(slug: params['slug'])
+      if category.save
+        halt 201, {message: 'created'}.to_json
+      else
+        halt 422, {errors: category.errors.messages.values.flatten}.to_json
+      end
     end
 
+    # @see https://github.com/jdr-tools/categories/wiki/Deleting-a-category
     declare_route 'delete', '/:id' do
       category = Arkaan::Permissions::Category.where(id: params[:id]).first
       if category.nil?
@@ -18,14 +26,10 @@ module Controllers
       end
     end
 
-    declare_route 'post', '/' do
-      check_presence 'slug'
-      category = Arkaan::Permissions::Category.new(slug: params['slug'])
-      if category.save
-        halt 201, {message: 'created'}.to_json
-      else
-        halt 422, {errors: category.errors.messages.values.flatten}.to_json
-      end
+    # @see https://github.com/jdr-tools/categories/wiki/Getting-the-list-of-categories
+    declare_route 'get', '/' do
+      categories = Decorators::Category.decorate_collection(Arkaan::Permissions::Category.all)
+      halt 200, {count: Arkaan::Permissions::Category.count, items: categories.map(&:to_h)}.to_json
     end
   end
 end
